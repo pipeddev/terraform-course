9dd56db7fc426b3b73c2b5b75f2ab74ab18ae603
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2025-05-08 22:01:40 -0400

    feat: practica with aws creating buckets

---
+++ practica_3/bucket.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "proveedores" {
+  count = 6
+  bucket = "proveedores-${random_string.sufijo[count.index].id}"
+
+  tags = {
+    Owner        = "Pipe"
+    Environment = "Dev"
+    Office      = "Prooveedores"
+  }
+}
+
+resource "random_string" "sufijo" {
+  count            = 6
+  length           = 8
+  special          = false
+  upper            = false
+  numeric          = false
+}
