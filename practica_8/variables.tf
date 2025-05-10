51e4fd319b599c03e2cc8a3e5af4b728a3f4c87a
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2025-05-10 13:57:04 -0400

    feat: first practice variable and aws

---
+++ practica_8/variables.tf
@@ -0,0 +1,14 @@
+variable "virginia_cidr" {
+  description = "CIDR Virginia"
+  type        = string
+}
+
+variable "public_subnet" {
+  description = "CIDR Public Subnet"
+  type        = string
+}
+
+variable "private_subnet" {
+  description = "CIDR Private Subnet"
+  type        = string
+}
