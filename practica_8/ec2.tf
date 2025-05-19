065e5ad028737781fa521d5df6e1f135290c7e91
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2025-05-18 21:17:07 -0400

    feat: documentation of object lifecycle in ec2.tf

--- practica_8/ec2.tf
+++ practica_8/ec2.tf
@@ -4,4 +4,20 @@ resource "aws_instance" "public_instance" {
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
   key_name      = data.aws_key_pair.key.key_name
+
+  lifecycle {
+    /* Esto indica que, si Terraform necesita reemplazar el recurso (por ejemplo, por un cambio en una propiedad inmutable), 
+                          primero creará el nuevo recurso y solo después destruirá el antiguo. Así se evita tiempo de inactividad.*/
+    #create_before_destroy = true 
+    /* Esto evita que el recurso sea destruido por accidente. Si intentas destruirlo (por ejemplo, con terraform destroy), 
+                Terraform lanzará un error y no permitirá la destrucción del recurso.*/
+    #prevent_destroy = true 
+    /* Esto indica que Terraform ignorará los cambios en las propiedades especificadas. 
+                Esto puede ser útil si sabes que un recurso va a cambiar fuera de Terraform y no quieres que eso cause un cambio en tu infraestructura. */
+    #ignore_changes = [ ] 
+
+    /* Esto indica que Terraform no debe intentar reemplazar el recurso si se producen cambios en las propiedades especificadas. 
+      Esto puede ser útil si sabes que un recurso va a cambiar fuera de Terraform y no quieres que eso cause un cambio en tu infraestructura. */
+    #replace_triggered_by = [  ]
+  }
 }
