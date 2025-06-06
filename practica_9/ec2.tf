d082619aebf895bef842b1e41aa1e43c03741dff
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2025-06-06 00:12:42 -0400

    feat: count vs for_each

---
+++ practica_9/ec2.tf
@@ -0,0 +1,22 @@
+# #ami-0f88e80871fd81e91
+
+variable "instancias" {
+  description = "Nombre de instancias"
+  #type = list(string) sirve para ser usado con count o hacer un parse en la instancia.
+  type = set(string)
+  default = [  "mysql", "jumpserver" ]
+}
+resource "aws_instance" "public_instance" {
+  #count         = length(var.instancias)
+  for_each      = var.instancias # si fuese lista podemos parsear con toset(var.instancias)
+  ami           = var.ec2_specs.ami
+  instance_type = var.ec2_specs.instance_type
+  subnet_id     = aws_subnet.public_subnet.id
+  key_name      = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_public_instance.id]
+  user_data = file("scripts/userdata.sh") # Archivo de script de usuario para inicialización
+
+  tags = {
+    "Name" = each.value
+  }
+}
